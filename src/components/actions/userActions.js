727f08eca20854d495d416ff793dc08f18945ec8
Shane Lupton <slupton89@icloud.com>
2019-02-19 16:45:53 -0700

    added user actions and reducers for store

---
+++ src/components/actions/userActions.js
@@ -0,0 +1,14 @@
+
+export const GET_USER_NAME = 'GET_USER_NAME'
+export const GetUserName = () => ({
+  type: GET_USER_NAME
+})
+
+export const SET_USER_NAME = 'SET_USER_NAME'
+export const setUserName = () => ({
+  type: SET_USER_NAME
+})
+
+export const getUser = () => (dispatch, getState) => {
+  dispatch(GetUserName())
+}
--- src/components/reducers/userReducer.js
+++ src/components/reducers/userReducer.js
@@ -1,7 +1,6 @@
 import {
-  FETCH_USER_REQUEST,
-  FETCH_USER_SUCCESS,
-  FETCH_USER_FAILURE,
+  GET_USER_NAME,
+  SET_USER_NAME
 } from '../actions/userActions.js'
 
 const initialState = {
@@ -11,18 +10,16 @@ const initialState = {
 }
 
 export const userReducer = (state = initialState, action) => {
-  if(action.type === FETCH_USER_REQUEST) {
+  if(action.type === GET_USER_NAME) {
     return Object.assign({}, state, {
       loading: true
     })
-  } else if(action.type === FETCH_USER_SUCCESS) {
+  } else if(action.type === SET_USER_NAME) {
     return Object.assign({}, state, {
       loading: false,
       error: null,
     })
-  } else if(action.type === FETCH_USER_FAILURE) {
-    return Object.assign({}, state, {
-      loading: true
-    })
+  } else {
+    return initialState
   }
 }
