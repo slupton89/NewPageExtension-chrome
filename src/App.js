a1cb574690886c6b914bec6bbdfd24b8f4e966c5
Shane Lupton <slupton89@icloud.com>
2019-02-19 16:17:32 -0700

    initial page rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section class="hero">
+          <div class="hero-inner">
+            <h1>My Awesome Website</h1>
+            <h2>Look at this Website and bask in its amazing glory!</h2>
+            <a href="https://google.com" class="btn">Go ahead...</a>
+          </div>
+        </section>
       </div>
     );
   }
