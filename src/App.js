288f053edff5ede04324d83c2ef11f23a394566f
Shane Lupton <slupton89@icloud.com>
2019-02-20 17:26:57 -0700

    set up project to use sass for styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import './App.css';
+import './styles/css/app.css';
 
 import Greeting from './components/Greeting'
 
