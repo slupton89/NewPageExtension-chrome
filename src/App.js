67a0435f1c5e04fea1e865d5fc6b2728c5bc1e13
Shane Lupton <slupton89@icloud.com>
2019-02-20 17:10:23 -0700

    general widget layout done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
+library.add(fas)
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <section class="hero">
-          <div class="hero-inner">
+        <section className="hero">
+          <div className="hero-inner">
+            {/* TODO replace with components */}
             <div className="greetingSection">
               <h1>Good</h1>
               {/* TODO replace */}
@@ -14,12 +19,30 @@ class App extends Component {
               {/* TODO replace */}
               <h1 className="greetingName">Shane</h1>
             </div>
-
-            {/* TODO show weather if location allowed */}
-            <div className="weatherSection">
-              <h2>Weather info</h2>
+            <div className="widgetSection">
+              {/* TODO replace with components */}
+              {/* TODO show weather if location allowed */}
+              <div className="weatherSection widget">
+                <FontAwesomeIcon icon="cloud-sun" className="icon" />
+                <h2>Weather info</h2>
+              </div>
+              {/* TODO replace with components */}
+              <div className="todaySection widget">
+                <FontAwesomeIcon icon="calendar-day" className="icon" />
+                <h2>Today</h2>
+              </div>
+              {/* TODO replace with components */}
+              <div className="widget"></div>
+              <div className="widget"></div>
+              <div className="widget"></div>
+              <div className="widget"></div>
+              <div className="widget"></div>
+              <div className="widget"></div>
+              <div className="widget"></div>
+              <div className="widget"></div>
+              <div className="widget"></div>
+              <div className="widget"></div>
             </div>
-            <h2>Look at this Website and bask in its amazing glory!</h2>
           </div>
         </section>
       </div>
