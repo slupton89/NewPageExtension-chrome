20b670a637bcabd805063d575a8b228d7b0f36bb
Unknown <slupton89@icloud.com>
2019-03-06 18:35:30 -0700

    Added FirstSetup component

---
+++ src/components/First-Setup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function FirstSetup() {
+  return (
+    <div className='setup'>
+      <h1 className='intro'>Welcome to zentab</h1>
+      <h2 className='intro'>Lets get started on personalizing your new tab</h2>
+    </div>
+  )
+}
