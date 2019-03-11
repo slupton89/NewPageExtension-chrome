5282f30f33f65e8ed3705ad2f0ed770e4d4e828e
Unknown <slupton89@icloud.com>
2019-03-10 20:13:45 -0600

    Created and styled backgrounds setup, ready for api on server

--- src/components/setup/Backgrounds.js
+++ src/components/setup/Backgrounds.js
@@ -16,11 +16,21 @@ export class Backgrounds extends React.Component {
       <div className={this.props.className} style={this.props.style}>
         <h1 className='title'>What backgrounds would you like to see?</h1>
         <div className='img-container'>
-          <div className='image'></div>
-          <div className='image'></div>
-          <div className='image'></div>
-          <div className='image'></div>
+          <div className='img-group'>
+            <h3 className='img-title'>Landscapes</h3>
+          </div>
+          <div className='img-group'>
+            <h3 className='img-title'>Space</h3>
+          </div>
+          <div className='img-group'>
+            <h3 className='img-title'>Cities</h3>
+            <img className='img' src='' alt='' />
+          </div>
+          <div className='img-group'>
+            <h3 className='img-title'>Space</h3>
+          </div>
         </div>
+        <FontAwesomeIcon className='nextBtn' icon="play-circle" />
       </div>
     )
 
